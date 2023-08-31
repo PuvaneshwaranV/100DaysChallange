0b54c135375a2bbd93a442608fc469259caf727a
PuvaneshwaranV <vpuvaneshwaran5@gmail.com>
2023-08-31 22:24:12 +0530

    add CmpStr

---
+++ CmpStr.java
@@ -0,0 +1,12 @@
+public class CmpStr
+{
+	public static void main(String[] args)
+	{
+		String s1="Puviiiiiii";
+		String s2="Puviiiiiii";
+		if(s1==s2)
+			System.out.println("Strings matched");
+		else
+			System.out.println("Strings not matched");
+	}
+}
