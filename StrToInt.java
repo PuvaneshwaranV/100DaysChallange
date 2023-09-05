0980b49356e2d11b1271885416eff65aa6a450b9
PuvaneshwaranV <vpuvaneshwaran5@gmail.com>
2023-09-05 20:56:10 +0530

    Commit strtoint

---
+++ StrToInt.java
@@ -0,0 +1,12 @@
+class StrToInt 
+{
+ 	public static void main(String[] args) 
+	{
+    		char a = '5';
+    		char b = '3';   
+    		int num1 = Character.getNumericValue(a);
+    		int num2 = Character.getNumericValue(b);
+    		System.out.println(num1);    
+    		System.out.println(num2);    
+  	}
+}
