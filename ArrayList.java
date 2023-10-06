79017223427557d87c490d2a363610646cf53025
PuvaneshwaranV <vpuvaneshwaran5@gmail.com>
2023-10-06 21:26:12 +0530

    commit ArrayList

---
+++ ArrayList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class Main 
+{
+  	public static void main(String[] args)
+	{
+    		ArrayList<String> languages = new ArrayList<>();
+    		languages.add("Java");
+    		languages.add("Python");
+    		languages.add("Swift");
+    		System.out.println("ArrayList: " + languages);
+  	}
+}
