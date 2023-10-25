4530e67274174d9cbc29db4c9e4729ab1a8ce661
PuvaneshwaranV <vpuvaneshwaran5@gmail.com>
2023-10-25 21:42:51 +0530

    commit FrequencyCharacter

---
+++ FrequencyCharacter.java
@@ -0,0 +1,17 @@
+public class FrequencyCharacter 
+{
+	public static void main(String[] args) 
+	{
+        		String str = "This website is awesome.";
+        		char ch = 'e';
+        		int frequency = 0;
+        		for(int i = 0; i < str.length(); i++) 
+		{
+            			if(ch == str.charAt(i)) 
+			{
+                				++frequency;
+            			}
+        		}
+        		System.out.println("Frequency of " + ch + " = " + frequency);
+    	}
+}
