f504315be63c633c0068d466baacea5451fdd169
PuvaneshwaranV <vpuvaneshwaran5@gmail.com>
2023-11-02 20:39:43 +0530

    CurrentDateTime commit

---
+++ CurrentDateTime.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+public class CurrentDateTime 
+{
+	public static void main(String[] args) 
+	{
+        		LocalDateTime current = LocalDateTime.now();
+        		System.out.println("Current Date and Time is: " + current);
+    	}
+}
