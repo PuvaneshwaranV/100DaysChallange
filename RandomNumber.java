cd9e5214c1e988715f0badce3f0763d0d898e78e
PuvaneshwaranV <vpuvaneshwaran5@gmail.com>
2023-11-07 21:12:33 +0530

    Commit RandomNumber

---
+++ RandomNumber.java
@@ -0,0 +1,11 @@
+import java.lang.Math;   
+public class RandomNumber  
+{   
+	public static void main(String args[])   
+	{   
+		System.out.println("1st Random Number: " + Math.random());   
+		System.out.println("2nd Random Number: " + Math.random());  
+		System.out.println("3rd Random Number: " + Math.random());   
+		System.out.println("4th Random Number: " + Math.random());   
+	}   
+} 
