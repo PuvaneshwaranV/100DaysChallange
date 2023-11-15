abfd04ee68440cf347ac8c80ddc2d863c66fd531
Puvaneshwaran V <142730682+PuvaneshwaranV@users.noreply.github.com>
2023-11-15 23:10:29 +0530

    Object Creation

---
+++ Object.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Object
+{
+	String name="Hello World";
+	public static void main(String args[])
+	{
+		Object obj=new Object();
+		System.out.print("object name :"+obj.name);
+	}
+}
